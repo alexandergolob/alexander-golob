a45e92aa7d2054924ea06dce8ec4abae05db12d6
kbaig <khizermbaig@gmail.com>
2019-02-20 11:33:17 -0600

    deleting more stuff before front page redesign

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,4 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import HeaderImageCarousel from '../components/HeaderImageCarousel';
-import Layout from '../components/Layout';
-import News from '../components/News';
-
-const HeroStatementContainer = styled.div`
-  text-align: center;
-`;
-
-const HeroStatement = styled.h1`
-  margin: 30px 0;
-  display: inline-block;
-  font-family: 'Special Elite', cursive;
-  font-size: 3rem;
-  color: #fff;
-  text-decoration: underline;
-  background: #000;
-`;
-
 export default () => <Layout>home page</Layout>;

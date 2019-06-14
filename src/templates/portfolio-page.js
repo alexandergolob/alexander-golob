77174a8e83e9b470eedf684fc12bc5fe5717f6b1
Kaz Baig <khizermbaig@gmail.com>
2019-06-14 11:43:01 -0500

    initial setup

--- src/templates/portfolio-page.js
+++ src/templates/portfolio-page.js
@@ -1,94 +1,41 @@
 import React from 'react';
 // import styled from 'styled-components';
-// import { graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 
-// import Layout from '../components/Layout';
-// import FrameBox from '../components/FrameBox';
+import Layout from '../components/Layout';
+import Carousel from '../components/Carousel';
 
-// const HeadingContainer = styled.h1`
-//   margin-bottom: 20px;
-//   text-align: center;
-// `;
+export const Template = ({ images }) => (
+  <Layout>
+    <Carousel images={images} />
+  </Layout>
+);
 
-// const Heading = styled(FrameBox)`
-//   display: inline-block;
-//   padding: 10px 40px;
-// `;
+export default ({ data: { portfolio } }) => (
+  <Template
+    {...portfolio.frontmatter}
+    images={portfolio.frontmatter.carouselImages.map(carouselImage => ({
+      ...carouselImage,
+      image: carouselImage.image.childImageSharp.fluid
+    }))}
+  />
+);
 
-// const Carousel = styled.div`
-//   margin-bottom: 20px;
-//   height: 450px;
-//   background: green;
-// `;
-
-// const Projects = styled.div`
-//   display: grid;
-//   grid-template-columns: 1fr 1fr 1fr;
-//   grid-auto-rows: 300px;
-//   grid-column-gap: 5px;
-//   grid-row-gap: 15px;
-// `;
-
-// const UnstyledProject = ({ title, ...rest }) => (
-//   <div {...rest}>
-//     <ProjectTitle>{title}</ProjectTitle>
-//   </div>
-// );
-
-// const Project = styled(UnstyledProject)`
-//   position: relative;
-//   background: green;
-//   border: 1px solid #000;
-// `;
-
-// const ProjectTitle = styled.div`
-//   position: absolute;
-//   bottom: 0;
-//   left: 0;
-//   right: 0;
-//   background: hsl(0, 0%, 93%);
-//   padding: 5px 0;
-//   font-size: 1.2rem;
-//   font-weight: 900;
-//   text-align: center;
-// `;
-
-// export const PortfolioPageTemplate = ({ heading }) => (
-//   <Layout>
-//     <HeadingContainer>
-//       <Heading>{heading}</Heading>
-//     </HeadingContainer>
-
-//     <Carousel />
-//     <Projects>
-//       <Project title='Kaleidoscope' />
-//       <Project title='Kaleidoscope' />
-//       <Project title='Kaleidoscope' />
-//       <Project title='Kaleidoscope' />
-//       <Project title='Kaleidoscope' />
-//       <Project title='Kaleidoscope' />
-//       <Project title='Kaleidoscope' />
-//       <Project title='Kaleidoscope' />
-//       <Project title='Kaleidoscope' />
-//       <Project title='Kaleidoscope' />
-//       <Project title='Kaleidoscope' />
-//       <Project title='Kaleidoscope' />
-//     </Projects>
-//   </Layout>
-// );
-
-// export default ({ data }) => (
-//   <PortfolioPageTemplate {...data.markdownRemark.frontmatter} />
-// );
-
-// export const query = graphql`
-//   query($id: String!) {
-//     markdownRemark(id: { eq: $id }) {
-//       frontmatter {
-//         heading
-//       }
-//     }
-//   }
-// `;
-
-export default () => <div>portfolio</div>;
+export const query = graphql`
+  query($id: String!) {
+    portfolio: markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        carouselImages {
+          description
+          image {
+            childImageSharp {
+              fluid(maxWidth: 1050) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;

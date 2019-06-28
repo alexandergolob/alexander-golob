1811a319e125c36fc5d4b34441cbcbc203e94613
Kaz Baig <khizermbaig@gmail.com>
2019-06-28 11:59:27 -0500

    404 page pending review

--- src/templates/404-page.js
+++ src/templates/404-page.js
@@ -1,30 +1,125 @@
 import React from 'react';
 import styled from 'styled-components';
 import { graphql } from 'gatsby';
+import GatsbyImage from 'gatsby-image';
 
+import { media } from '../components/ThemeProvider';
 import Layout from '../components/Layout';
+import UnstyledFinalCTA from '../components/FinalCTA';
+import InternalLink from '../components/InternalLink';
 
-const Message = styled.div`
-  margin-top: 3em;
+const Container = styled.div`
+  background: ${props => props.theme.colors.offLight};
+  border: ${props => props.theme.misc.frameBorder};
+  padding: 2em 3em;
+  font-family: ${props => props.theme.fonts.serif};
+
+  ${media.tablet`padding: 1em 1.5em; font-size: 0.8em;`}
+  ${media.mobile`padding: 1em; font-size: 0.6em;`}
+`;
+
+const Title = styled.h1`
   font-size: 2em;
   text-align: center;
-  font-family: ${props => props.theme.fonts.serif};
   font-weight: 600;
 `;
 
-export const Template = ({ message }) => (
+const Subtitle = styled(Title).attrs({ as: 'div' })``;
+
+const PreImage = styled(Subtitle)`
+  margin: 1em 0;
+  font-size: 1.75em;
+`;
+
+const Image = styled(GatsbyImage)`
+  margin: auto;
+  max-width: 500px;
+  height: 250px;
+
+  ${media.mobile`height: 200px;`}
+`;
+
+const PreLink = styled(PreImage)`
+  margin-bottom: 0;
+`;
+
+const PreContact = styled(PreImage)`
+  margin-bottom: 0.5em;
+`;
+
+const FinalCTAContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const FinalCTA = styled(UnstyledFinalCTA)`
+  font-size: 1.5em;
+  ${media.mobile`width: 100%; padding: 0.25em 1em;`}
+`;
+
+const Links = styled.div`
+  font-size: 1.75em;
+  text-align: center;
+  font-weight: 400;
+`;
+
+const PageLink = styled(InternalLink)``;
+
+export const Template = ({
+  title,
+  subtitle,
+  preImage,
+  image,
+  preLink,
+  preContact
+}) => (
   <Layout>
-    <Message>{message}</Message>
+    <Container>
+      <Title>{title}</Title>
+      <Subtitle>{subtitle}</Subtitle>
+      <PreImage>{preImage}</PreImage>
+      <Image fluid={image} alt='' />
+      <PreLink>{preLink}</PreLink>
+      <Links>
+        <PageLink to='/'>Home</PageLink>
+        {' - '}
+        <PageLink to='/portfolio'>Portfolio</PageLink>
+        {' - '}
+        <PageLink to='/art'>Art</PageLink> -<PageLink to='/blog'>Blog</PageLink>
+        {' - '}
+        <PageLink to='/about'>About</PageLink>
+      </Links>
+      <PreContact>{preContact}</PreContact>
+      <FinalCTAContainer>
+        <FinalCTA />
+      </FinalCTAContainer>
+    </Container>
   </Layout>
 );
 
-export default ({ data }) => <Template {...data.markdownRemark.frontmatter} />;
+export default ({ data }) => (
+  <Template
+    {...data.markdownRemark.frontmatter}
+    image={data.markdownRemark.frontmatter.image.childImageSharp.fluid}
+  />
+);
 
 export const query = graphql`
   query($id: String!) {
     markdownRemark(id: { eq: $id }) {
       frontmatter {
-        message
+        title
+        subtitle
+        preImage
+        image {
+          childImageSharp {
+            fluid(maxWidth: 500) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        preLink
+        preContact
       }
     }
   }

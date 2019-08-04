d68d0c32b606b08a55154bc7798524519725ed23
Kaz Baig <khizermbaig@gmail.com>
2019-08-04 18:17:53 -0500

    replaced placeholder icons with social icons

--- src/components/PostShareIcons.js
+++ src/components/PostShareIcons.js
@@ -1,34 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faLink } from '@fortawesome/free-solid-svg-icons';
-import {
-  faFacebookF,
-  faLinkedinIn,
-  faTwitter
-} from '@fortawesome/free-brands-svg-icons';
+import UnstyledSocialIcons from './SocialIcons';
 
-const Container = styled.div`
+const SocialIcons = styled(UnstyledSocialIcons)`
   display: flex;
   justify-content: space-between;
   align-items: center;
   font-size: 22px;
-`;
-
-const Icon = styled(FontAwesomeIcon)`
-  margin-right: 10px;
-  &:last-of-type {
-    margin-right: 0;
+  > * {
+    margin-right: 10px;
+    &:last-of-type {
+      margin-right: 0;
+    }
   }
 `;
 
-export default ({ ...rest }) => (
-  <Container {...rest}>
-    <Icon icon={faEnvelope} />
-    <Icon icon={faLink} />
-    <Icon icon={faFacebookF} />
-    <Icon icon={faLinkedinIn} />
-    <Icon icon={faTwitter} />
-  </Container>
-);
+export default ({ ...rest }) => <SocialIcons {...rest} />;

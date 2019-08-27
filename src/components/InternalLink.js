cbe850384a9baae505ad8b47fa3c2c4dc8416e7f
Kaz Baig <khizermbaig@gmail.com>
2019-08-26 23:44:44 -0500

    relative images package link collisions fix

--- src/components/InternalLink.js
+++ src/components/InternalLink.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
 
-const InternalLink = styled(Link).attrs(props => ({
-  to: props.to && props.to.replace('../pages', '')
-}))`
+const InternalLink = styled(Link)`
   color: inherit;
   text-decoration: none;
 `;
 
-export default InternalLink;
+export default ({ to, ...rest }) => (
+  <InternalLink to={to && to.replace('../pages', '')} {...rest} />
+);
